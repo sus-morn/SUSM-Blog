8750b074ce5687661b816bc097d5fcf3fa6eee4e
Chenric <91937041+ChenXu233@users.noreply.github.com>
2024-07-30 20:43:15 +0800

    :sparkles: 后端简单功能完成

--- backend/__init__.py
+++ backend/__init__.py
@@ -1,7 +1,27 @@
-from fastapi import FastAPI
+from fastapi import FastAPI,Depends
 
+from database import session_local, engine, User, Article
 app = FastAPI(title="SUSM Blog", description="A simple blog API", version="0.0.1")
+
+# 未来移入Dependency
+def get_db():
+    db = session_local()
+    try:
+        yield db
+    finally:
+        db.close()
 
 @app.get("/")
 async def root():
     return {"message": "Welcome to SUSM Blog API!"}
+
+@app.post("/article/post")
+async def post_article( content: str, db = Depends(get_db)):
+    db.add(Article(title="test", content=content, author_id=1))
+    db.commit()
+    return content
+
+@app.get("/article/{article_id}")
+async def get_article(article_id: int, db = Depends(get_db)):
+    data = db.query(Article).filter(Article.id == article_id).first()
+    return data.content
